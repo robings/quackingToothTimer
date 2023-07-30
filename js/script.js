15abf1c32890d47ca6a952983fc36c0dd863d72f
robings <58939401+robings@users.noreply.github.com>
2023-07-30 23:02:35 +0100

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,116 @@
+const alarmSound = new Audio();
+const alarmSoundSrc = "./sound/075176_duck-quack-40345.mp3";
+let timerButtonFunction = "start";
+let timerInterval;
+let alarmQuackInterval;
+let soundTimer = true;
+
+function firstClick() {
+  alarmSound.load();
+  document.querySelector("body").removeEventListener("click", firstClick);
+}
+
+function changeDuckSize(imageSize) {
+  var duckImage = document.getElementById("duck");
+  var existingWidth = duckImage.style.width;
+  setTimeout(() => {
+    duckImage.style.width = imageSize;
+  }, 300);
+  setTimeout(() => {
+    duckImage.style.width = existingWidth;
+  }, 600);
+}
+
+function playSound() {
+  alarmSound.src = alarmSoundSrc;
+  alarmSound.currentTime = 0;
+  alarmSound.play();
+  changeDuckSize("260px");
+}
+
+function createCountdownDisplay(timeLeftInSeconds) {
+  let seconds = timeLeftInSeconds % 60;
+  let minutes = (timeLeftInSeconds - seconds) / 60;
+  let secondsToDisplay = seconds < 10 ? "0" + seconds : seconds;
+  let minutesToDisplay = minutes < 10 ? "0" + minutes : minutes;
+  document.getElementById(
+    "timerDisplay"
+  ).textContent = `${minutesToDisplay}:${secondsToDisplay}`;
+}
+
+document.querySelector("body").addEventListener("click", firstClick);
+document.querySelector("#startButton").addEventListener("click", controlTimer);
+
+function alarmQuack(numberOfQuacks, intervalInMilliseconds, isEnding) {
+  var i = 0;
+  playSound();
+  i++;
+  if (numberOfQuacks > 1) {
+    alarmQuackInterval = setInterval(() => {
+      playSound();
+      if (i === numberOfQuacks - 1) {
+        clearTimeout(alarmQuackInterval);
+        if (isEnding) {
+          restoreStartButton();
+        }
+      }
+      i++;
+    }, intervalInMilliseconds);
+  }
+}
+
+function restoreStartButton() {
+  timerButtonFunction = "start";
+  var controlButton = document.querySelector("#startButton");
+  controlButton.textContent = "Start Tooth Timer";
+  controlButton.style.backgroundColor = "#156734";
+  document.getElementById("timerDisplay").textContent = "02:00";
+}
+
+function stopTimer(cancelling) {
+  clearTimeout(timerInterval);
+  if (cancelling) {
+    clearTimeout(alarmQuackInterval);
+    restoreStartButton();
+  }
+}
+
+function startTimer() {
+  let minutes = 2;
+  let seconds = minutes * 60;
+  // for testing
+  // let seconds = 2;
+  let remainingS = 120;
+  let interval = 20;
+
+  let startTime = new Date();
+
+  createCountdownDisplay(seconds);
+
+  timerInterval = setInterval(() => {
+    let timeNow = new Date();
+    let timeDifference = Math.floor((timeNow - startTime) / 1000);
+    remainingS = seconds - timeDifference;
+
+    createCountdownDisplay(remainingS);
+    if (remainingS < 1) {
+      stopTimer();
+      alarmQuack(10, 1250, true);
+    } else if (remainingS % interval === 0 && remainingS > 0) {
+      alarmQuack(remainingS === 60 ? 2 : 1, 1000);
+    }
+  }, 1000);
+
+  timerButtonFunction = "cancel";
+  var controlButton = document.querySelector("#startButton");
+  controlButton.textContent = "Cancel Tooth Timer";
+  controlButton.style.backgroundColor = "#e81123";
+}
+
+function controlTimer() {
+  if (timerButtonFunction === "start") {
+    startTimer();
+  } else if (timerButtonFunction === "cancel") {
+    stopTimer(true);
+  }
+}
